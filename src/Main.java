2df764219d0c9da5c77fb754a204003bd5cce20a
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-06 14:21:54 +0200

    fix for scanner

--- src/Main.java
+++ src/Main.java
@@ -20,40 +20,46 @@ public class Main {
 
     private static void setItems() {
         Scanner scanner = new Scanner(System.in);
-        Scanner input = new Scanner(System.in);
         for (int i = 0; i < multimediaItems.length; i++) {
             String selection;
             String title;
-            int duration = 0;
+            String duration;
             System.out.println("Create new media");
-            System.out.println("please select the media type");
-            System.out.println("1 - Image");
-            System.out.println("2 - Audio");
-            System.out.println("3 - Video");
-            selection = scanner.nextLine();
+            do {
+                System.out.println("please select the media type");
+                System.out.println("1 - Image");
+                System.out.println("2 - Audio");
+                System.out.println("3 - Video");
+                selection = scanner.nextLine();
+            } while (!selection.matches("[123]"));
             System.out.println("enter title");
             title = scanner.nextLine();
+
+
             switch (selection) {
                 case "1": {
                     multimediaItems[i] = new Image(title);
                     break;
                 }
                 case "2": {
-                    System.out.println("enter duration sec");
-                    duration = input.nextInt();
-                    multimediaItems[i] = new Audio(title, duration);
+                    do {
+                        System.out.println("enter duration sec");
+                        duration = scanner.nextLine();
+                    } while (!duration.matches("^\\d+$"));
+                    multimediaItems[i] = new Audio(title, Integer.parseInt(duration));
                     break;
                 }
                 case "3": {
-                    System.out.println("enter duration sec");
-                    duration = input.nextInt();
-                    multimediaItems[i] = new Video(title, duration);
+                    do {
+                        System.out.println("enter duration sec");
+                        duration = scanner.nextLine();
+                    } while (!duration.matches("^\\d+$"));
+                    multimediaItems[i] = new Video(title, Integer.parseInt(duration));
                     break;
                 }
             }
         }
         scanner.close();
-        input.close();
     }
 
     private static void printItems() {
