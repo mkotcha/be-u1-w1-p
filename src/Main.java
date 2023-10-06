533d2a1ff2bbdb1e7b34b658249563f8af6900f0
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-06 12:57:31 +0200

    workink on scan

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,65 @@
+import Multimedia.Audio;
+import Multimedia.Image;
+import Multimedia.MultimediaItem;
+import Multimedia.Video;
+
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    static MultimediaItem[] multimediaItems = new MultimediaItem[2];
+
     public static void main(String[] args) {
-        // Press Ctrl+. with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+F5 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        setItems();
+        printItems();
 
-            // Press F5 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing F9.
-            System.out.println("i = " + i);
+    }
+
+    private static void setItems() {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < multimediaItems.length; i++) {
+            String selection;
+            String title;
+            int duration;
+            System.out.println("Create new media");
+            System.out.println("please select the media type");
+            System.out.println("1 - Image");
+            System.out.println("2 - Audio");
+            System.out.println("3 - Video");
+            selection = scanner.nextLine();
+            System.out.println("enter title");
+            title = scanner.nextLine();
+            switch (selection) {
+                case "1" -> {
+                    multimediaItems[i] = new Image(title);
+                    break;
+                }
+                case "2" -> {
+                    System.out.println("enter duration sec");
+                    duration = scanner.nextInt();
+                    multimediaItems[i] = new Audio(title, duration);
+                    break;
+                }
+                case "3" -> {
+                    System.out.println("enter duration sec");
+                    duration = scanner.nextInt();
+                    multimediaItems[i] = new Video(title, duration);
+                    break;
+                }
+            }
+        }
+        scanner.close();
+    }
+
+    private static void printItems() {
+        for (MultimediaItem item : multimediaItems) {
+            if (item instanceof Image) ((Image) item).show();
+            if (item instanceof Audio) ((Audio) item).play();
+            if (item instanceof Video) ((Video) item).play();
         }
     }
+
 }
